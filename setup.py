63b7ffc80e30b250bdffead10b70482ff0b6036c
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-04-08 04:40:22 +0700

    update setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="datetool",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "jsonargparse",
+        "pytz",
+        "sphinx",
+        "sphinx-argparse"
+    ],
+    entry_points={
+        'console_scripts': [
+            'datetools = datetools:main',
+        ],
+    },
+)
